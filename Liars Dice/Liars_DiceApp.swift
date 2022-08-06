a05efeea7c0bdeb97d2a12072d3080e7e24151f5
Robert Giesler <robert1020@me.com>
2022-08-06 13:47:00 +0200

    Initial Commit

---
+++ Liars Dice/Liars_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liars_DiceApp.swift
+//  Liars Dice
+//
+//  Created by Robert Giesler on 06.08.22.
+//
+
+import SwiftUI
+
+@main
+struct Liars_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
