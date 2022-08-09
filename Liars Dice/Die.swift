4660b71e6284988c53663f97d657d020a46cb4cf
Robert Giesler <robert1020@me.com>
2022-08-09 11:57:30 +0200

    Added Die class and Face enum.

---
+++ Liars Dice/Die.swift
@@ -0,0 +1,20 @@
+//
+//  Die.swift
+//  Liars Dice
+//
+//  Created by Robert Giesler on 06.08.22.
+//
+
+import Foundation
+
+class Die {
+    var face: Face
+    
+    func roll() {
+        self.face = Face(rawValue: Int.random(in: 1...6))!
+    }
+    
+    init() {
+        self.face = Face(rawValue: Int.random(in: 1...6))!
+    }
+}
---
+++ Liars Dice/Face.swift
@@ -0,0 +1,12 @@
+//
+//  Face.swift
+//  Liar's Dice
+//
+//  Created by Robert Giesler on 08.08.22.
+//
+
+import Foundation
+
+enum Face: Int {
+    case One = 1, Two, Three, Four, Five, Six
+}
